2607cba5cc67c233b513047c9fd25372ab1c4ac0
omranali2111 <aljabri1234@hotmail.com>
2023-07-12 13:12:10 +0400

    task1:takes a list of integers as input and removes duplicate numbers

---
+++ c-sharp-ListTasks/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] newArray = new int[10];
+        Console.WriteLine("enter list of numbers:");
+
+        task1(newArray);
+
+    }
+
+    public static void task1(IList<int> newArray)
+    {
+        /*
+        1.Write a program that takes a list of integers as input and removes
+            duplicate numbers, keeping only the unique elements. Implement a 
+            method that accepts the list and returns a new list containing 
+            only the unique elements in the original order.
+        */
+        int c = newArray.Count;
+        for (int i = 0; i < c; i++)
+        {
+            try
+            {
+                newArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        List<int> numbers = new List<int>();
+       
+        foreach (int i in newArray)
+        {
+            numbers.Add(i);
+           
+        }
+        Console.WriteLine("Original list:");
+        foreach (int number in numbers)
+        {
+           Console.Write(number+" ");
+        }
+        List<int> uniqueList = numbers.Distinct().ToList();
+
+        Console.WriteLine("\nList with duplicates removed:");
+
+        foreach (int number in uniqueList)
+        {
+            Console.Write(number + " ");
+        }
+
+
+
+
+    }
+}
+
